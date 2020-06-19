e420c0d63c26b7c09353db72053811702aff6cab
hakmad <39906682+hakmad@users.noreply.github.com>
2020-06-19 12:34:41 +0000

    Refactored gen.py to be more modular

--- gen.py
+++ gen.py
@@ -1,72 +1,70 @@
 #!/usr/bin/env python3
 
-import markdown
-
 from glob import glob
 from jinja2 import Template
+from markdown import Markdown
 
 class Post:
-    def __init__(self, title, date, content, filename):
+    def __init__(self, content, filename, title, date, post_type):
+        self.content = content
+        self.filename = filename[:-2]
         self.title = title
         self.date = date
-        self.content = content
-        self.filename = filename
+        self.post_type = post_type
 
     def render_html(self):
-        with open("templates/post.html", "r") as file:
-            template = Template(file.read())
+        if self.post_type == "content":
+            with open("templates/content.html", "r") as file:
+                template = Template(file.read())
+                self.html = template.render(content=self.content, title=self.title, date=self.date)
 
-        self.html = template.render(content = self.content, title = self.title, date = self.date)
+        elif self.post_type == "meta":
+            with open("templates/meta.html", "r") as file:
+                template = Template(file.read())
+                self.html = template.render(content=self.content, title=self.title)
 
-        with open(self.filename[:-2] + "html", "w") as file:
+    def write_to_file(self):
+        with open(self.filename + "html", "w") as file:
             file.write(self.html)
 
-posts = []
+def get_posts(post_path, post_type):
+    post_list = []
 
-for post in glob("./content/*.md"):
-    with open(post, "r") as file:
-        data = file.read()
+    for post in glob(post_path):
+        if post == "README.md":
+            pass
 
-        md = markdown.Markdown(extensions = ["meta", "fenced_code"])
+        else:
+            with open(post, "r") as file:
+                data = file.read()
+                md = Markdown(extensions=["meta", "fenced_code"])
+                content = md.convert(data)
+                title = md.Meta["title"].pop()
 
-        content = md.convert(data)
-        title = md.Meta["title"][0]
-        date = md.Meta["date"][0]
-        filename = file.name
+                try:
+                    date = md.Meta["date"].pop()
+                except KeyError:
+                    date = None
 
-        posts.append(Post(title, date, content, filename))
+                post_list.append(Post(content, file.name, title, date, post_type))
 
-posts.sort(key = lambda x: x.date, reverse = True)
+    return post_list
+
+def make_html(post_list):
+    for post in post_list:
+        post.render_html()
+        post.write_to_file()
+
+content_posts = get_posts("./content/*.md", "content")
+content_posts.sort(key=lambda post: post.date, reverse=True)
 
 with open("index.md", "w") as file:
-    file.write("# Index\n\n")
+    file.write("title: Index\n\n")
 
-for post in posts:
-    post.render_html()
+    for post in content_posts:
+        file.write("[{}]({}) ({})\n\n".format(post.title, post.filename + "html", post.date))
 
-    title = post.title
-    date = post.date
-    filename = post.filename[:-2] + "html"
+meta_posts = get_posts("*.md", "meta")
 
-    with open("index.md", "a") as file:
-        file.write("[{}]({}) ({})\n\n".format(title, filename, date))
-
-for post in glob("*.md"):
-    if post == "README.md":
-        pass
-    else:
-        with open(post, "r") as file:
-            data = file.read()
-            filename = file.name
-    
-            md = markdown.Markdown()
-            title = file.name[:-3].title()
-            content = md.convert(data)
-    
-        with open("templates/meta.html", "r") as file:
-            template = Template(file.read())
-    
-        html = template.render(content = content, title = title)
-    
-        with open(filename[:-2] + "html", "w") as file:
-            file.write(html)
+make_html(content_posts)
+make_html(meta_posts)
